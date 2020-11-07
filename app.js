cb265e3775d45ab9bca37ce021d1850a28a46049
Alessandra Burckhalter <ale.lptc@gmail.com>
2020-11-07 10:29:35 -0500

    new style

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ hamburger.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
+		header.style.backgroundColor = '#f8e5e5';
 	} else {
 		header.style.backgroundColor = 'transparent';
 	}
@@ -39,3 +39,5 @@ var wow = new WOW(
     }
   );
   wow.init();
+
+
